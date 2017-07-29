634c952ceeeb1432062faa518e75631fde176fc0
Gr3mlin106 <maciej.matuszewski@me.com>
2017-07-29 15:39:56 +0200

    [Improve] AppDelegate clean

--- Netguru Interview App/AppDelegate.swift
+++ Netguru Interview App/AppDelegate.swift
@@ -9,26 +9,21 @@
 import UIKit
 
 @UIApplicationMain
+/// System class for handle application states
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    ///Main window in application
     var window: UIWindow?
 
-
+    /// Method called when app did finish launch
+    ///
+    /// - Parameters:
+    ///   - application: singleton app object
+    ///   - launchOptions: A dictionary indicating the reason the app was launched (if any).
+    /// - Returns: false if the app cannot handle the URL resource or continue a user activity, otherwise return true. The return value is ignored if the app is launched as a result of a remote notification.
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FlowManager.shared.loadInitialController()
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {}
-
-    func applicationDidEnterBackground(_ application: UIApplication) {}
-
-    func applicationWillEnterForeground(_ application: UIApplication) {}
-
-    func applicationDidBecomeActive(_ application: UIApplication) {}
-
-    func applicationWillTerminate(_ application: UIApplication) {}
-
-
 }
 
